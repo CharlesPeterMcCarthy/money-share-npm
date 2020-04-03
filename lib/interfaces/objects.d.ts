a3fe7f218e8ec1c633503b2d25a03d58138ca84b
Charles McCarthy <charles@studybundles.com>
2020-04-03 12:53:34 +0100

    User, Subscription,DBItem

--- lib/interfaces/objects.d.ts
+++ lib/interfaces/objects.d.ts
@@ -1,2 +1,39 @@
-export interface EmptyInterface {
+export declare type UserType = 'User' | 'Admin';
+export interface DBItem {
+    pk: string;
+    sk: string;
+    sk2?: string;
+    sk3?: string;
+    entity: string;
+}
+export interface UserConnection {
+    deviceId: string;
+    connectionId: string;
+    connectedAt: string;
+}
+export interface User extends DBItem {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    avatar?: string;
+    email: string;
+    userType: UserType;
+    confirmed: boolean;
+    times: {
+        confirmedAt?: string;
+        createdAt: Date | string;
+        lastLogin?: Date | string;
+    };
+    connections: UserConnection[];
+}
+export interface Subscription extends DBItem {
+    connectionId: string;
+    subscriptionId: string;
+    itemType: string;
+    itemId: string;
+    deviceId: string;
+    userId?: string;
+    times: {
+        subscribedAt: string;
+    };
 }
